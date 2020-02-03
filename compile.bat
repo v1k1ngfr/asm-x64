e7e63d230a8a1aab4513d820aab4e40d0e806bc5
v1k1ngfr <32877266+v1k1ngfr@users.noreply.github.com>
2020-02-03 18:03:12 +0100

    Rename files and add disasm trace

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
-del x64.obj
-del x64.exe
-"C:\Program Files\NASM\nasm.exe" -f win64 x64_MessageBoxA.asm -o x64.obj
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64\link.exe" /ENTRY:main /MACHINE:X64 /NODEFAULTLIB /SUBSYSTEM:CONSOLE x64.obj
+del x64_MessageBoxA.obj
+del x64_MessageBoxA.exe
+"C:\Program Files\NASM\nasm.exe" -f win64 x64_MessageBoxA.asm -o x64_MessageBoxA.obj
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.14.26428\bin\Hostx64\x64\link.exe" /ENTRY:main /MACHINE:X64 /NODEFAULTLIB /SUBSYSTEM:CONSOLE x64_MessageBoxA.obj
+"C:\Program Files\NASM\ndisasm.exe" -b 64 x64.obj > x64_MessageBoxA_disasm.txt
+x64_MessageBoxA.exe
